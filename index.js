d938792f3949e6b428c709913a10ed6ddda57e90
Delvin <delvin.khor@splose.com>
2026-07-14 10:24:14 +0930

    test

--- index.js
+++ index.js
@@ -9,20 +9,6 @@ function parseGroupName(name) {
 
 async function run() {
   const output = await exec("npm run generate");
-  const suggestions = output.stdout.split("navigation object suggestion:")[1];
-  const endpoints = JSON.parse(suggestions);
-  for (const endpoint of endpoints) {
-    endpoint.group = parseGroupName(endpoint.group);
-  }
-
-  const mintJsonTemplate = fs.readFileSync("mint.json.template");
-  const mintJson = JSON.parse(mintJsonTemplate);
-  const apiReference = mintJson.navigation.find(
-    (g) => g.group === "API Reference"
-  );
-  apiReference.pages = endpoints;
-
-  fs.writeFileSync("mint.json", JSON.stringify(mintJson));
 }
 
 run();
